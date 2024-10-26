0919866c1bc7999719d702f36983a7c7cd884588
Gantt Fincher <gantt.fincher@gmail.com>
2024-10-26 13:18:08 -0500

    Added Synchronization System

--- send.cpp
+++ send.cpp
@@ -11,9 +11,20 @@ sockaddr_in servAddr;
     servAddr.sin_port = htons(8080);
     servAddr.sin_addr.s_addr = inet_addr(ip);
     int clientSoc = socket(AF_INET, SOCK_STREAM, 0);
-    connect(clientSoc, (struct sockaddr*)&servAddr, sizeof(servAddr));
-    send(clientSoc, msg, strlen(msg), 0);
-    close(clientSoc);
+    int sockSuccess = connect(clientSoc, (struct sockaddr*)&servAddr, sizeof(servAddr));
+    if (sockSuccess != 0) {
+        std::cout << "Connection not found, waiting on open port..." << std::endl;
+        while (sockSuccess != 0) {
+        sockSuccess = connect(clientSoc, (struct sockaddr*)&servAddr, sizeof(servAddr));
+        sleep(1);
+    }
+    }
+    if (sockSuccess == 0) {
+     send(clientSoc, msg, strlen(msg), 0);
+     close(clientSoc);
+     std::cout << "Message delivered!";
+    }
+    
 }
 int main(int argc, char* argv[]) {
     std::string msg,ipA;
@@ -21,6 +32,7 @@ int main(int argc, char* argv[]) {
             std::cout << "Please enter the IP address which you wish to connect to" << std::endl;
             std::cin >> ipA;
             std::cout << "Please enter the message which you wish to send" << std::endl;
+            std::cin.ignore();
             std::getline(std::cin, msg);
             
     }
