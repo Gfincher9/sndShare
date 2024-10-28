b1a78e86077069d5074fe46617f042a7ca35416e
Gantt Fincher <gantt.fincher@gmail.com>
2024-10-27 21:03:23 -0500

    Standardized arguments; Added file sharing capability

--- send.cpp
+++ send.cpp
@@ -9,6 +9,7 @@ void confFetch() {
     char buf[11];
     char newbuf[5];
     FILE *fp = fopen("snd.conf", "r");
+    //todo: add config file if uncreated
     fgets(buf, 11, fp);
     fclose(fp);
     for (int i = 5; i < strlen(buf); i++) {
@@ -16,7 +17,7 @@ void confFetch() {
     } 
     port = std::stoi(newbuf);
 }
-void socketConnect(const char* ip, const char* msg) {
+void socketConnect(const char* ip, const char* path) {
 sockaddr_in servAddr;
     servAddr.sin_family = AF_INET;
     servAddr.sin_port = htons(port);
@@ -31,31 +32,41 @@ sockaddr_in servAddr;
     }
     }
     if (sockSuccess == 0) {
-     send(clientSoc, msg, strlen(msg), 0);
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL) {
+        std::cout << "File not found";
+    }
+    else {
+    char *buf;
+    while (fgets(buf, 1024, fp)!=NULL) {
+     send(clientSoc, buf, strlen(buf), 0);
+    }
+     fclose(fp);
      close(clientSoc);
-     std::cout << "Message delivered!" << std::endl;
+     std::cout << "Data delivered!" << std::endl;
+    }
     }
     
 }
 int main(int argc, char* argv[]) {
     confFetch();
-    std::string msg,ipA;
-    if (argc < 2) {
+    std::string path,ipA;
+    if (argc == 1) {
             std::cout << "Please enter the IP address which you wish to connect to" << std::endl;
             std::cin >> ipA;
-            std::cout << "Please enter the message which you wish to send" << std::endl;
+            std::cout << "Please enter the path to the file which you wish to send" << std::endl;
             std::cin.ignore();
-            std::getline(std::cin, msg);
+            std::getline(std::cin, path);
             
     }
-    else if (argc<3) {
+    else if (argc == 2) {
             ipA = argv[1];
-            std::cout << "Please enter the message which you wish to send" << std::endl;
-            std::getline(std::cin, msg);
+            std::cout << "Please enter the path to the file which you wish to send" << std::endl;
+            std::getline(std::cin, path);
 }
         else {
-            ipA=argv[1];
-            msg=argv[2];
+            std::cout << "Invalid Argument count";
+            return 1;
         }
-    socketConnect(ipA.c_str(), msg.c_str());
+    socketConnect(ipA.c_str(), path.c_str());
 }
